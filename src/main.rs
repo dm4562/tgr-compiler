bc73100c2f450e3188d0273f37bebffcd504d9dc
Tam, Christopher <christophertam@gatech.edu>
2018-03-23 22:36:31 -0400

    Move scanner to separate package and rustify project 1 (#6)

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,15 @@
-extern crate regex;
 extern crate clap;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 
-use std::io::{Read};
-use std::collections::VecDeque;
-use regex::Regex;
-use std::fmt::Write;
+mod scanner;
+
 use clap::{Arg, App};
+use regex::Regex;
+use std::io::Read;
 use std::fs::File;
-
-
-#[derive(Debug)]
-struct Token<'a> {
-    val: &'a str,
-    token_name: &'a str,
-    index: usize,
-    length: usize,
-    token_type: usize
-}
-
-fn print_tokens(queue: &VecDeque<Token>, print_tokens: &bool) {
-    if !print_tokens {
-        return;
-    }
-
-    let mut output: String = String::new();
-    for token in queue {
-        if token.token_name == "keyword" {
-            write!(&mut output, "{} ", &token.val);
-        } else {
-            write!(&mut output, "{}:{} ", &token.val, &token.token_name);
-        }
-    }
-    output = output.trim().to_string();
-    print!("{}", &output);
-}
+use std::process;
 
 fn read_file(file_name: String) -> String {
     let comment = Regex::new(r"/\*(([^\*/])*)\*/").unwrap();
@@ -48,6 +24,7 @@ fn read_file(file_name: String) -> String {
 }
 
 fn main() {
+    // Argument parsing
     let matches = App::new("TigerCompiler")
         .version("0.1")
         .author("Christopher Tam and Dhruv Mehra")
@@ -61,116 +38,21 @@ fn main() {
             .help("Print tokens to stdout"))
         .get_matches();
 
-    let token = matches.is_present("tokens");
+    let should_print_tokens = matches.is_present("tokens");
     let file_name = matches.value_of("file").unwrap().to_string();
 
     let buffer = read_file(file_name);
 
-    let keyword1 = Regex::new(r"^((array)|(begin)|(boolean)|(break)|(do)|(else)|(end)|(enddo)|(endif)|(false)|(float)|(for)|(func)|(if)|(in)|(int)|(let)|(of)|(return)|(then)|(to)|(true)|(type)|(unit)|(var)|(while))$").unwrap();
-    let keyword2 = Regex::new(r"^(,|:|;|\(|\)|\[|\]|\{|\}|\.|\+|-|\*|/|=|<|>|<>|<=|>=|&|\||:=)$").unwrap();
-    let id = Regex::new(r"^((_[A-Za-z_0-9]*([0-9A-Za-z])[A-Za-z_0-9]*)|(([A-Za-z])[0-9A-Za-z_]*))$").unwrap();
-    let intlit = Regex::new(r"^(\d*)$").unwrap();
-    let floatlit = Regex::new(r"^(\d+\.\d*)$").unwrap();
+    // Step 1: run the scanner to parse the tokens
+    let tokens = match scanner::parse_tokens(&buffer) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1);
+        },
+    };
 
-    let regex_vec = vec![&floatlit, &intlit, &id, &keyword1, &keyword2];
-    let output_vec = vec!["floatlit", "intlit", "id", "keyword", "keyword"];
-
-    let (mut start, mut end) = (0, 1);
-    let mut match_found: i32 = -1;
-    let mut pre_match_found: i32 = -1;
-    let mut need_keyword2 = false;
-
-    let mut q: VecDeque<Token> = VecDeque::new();
-
-    while end <= buffer.len() {
-        if buffer[end - 1..end].eq(" ") {
-
-            if pre_match_found < 0 {
-                eprintln!("Syntax error 1");
-                print_tokens(&q, &token);
-                return;
-            }
-
-
-            if need_keyword2 && pre_match_found != 4 {
-                q.pop_back();
-                eprintln!("Syntax error 2");
-                print_tokens(&q, &token);
-                return;
-            }
-
-            if need_keyword2 && pre_match_found == 4 {
-                need_keyword2 = false;
-            }
-
-            let token = Token {
-                val: &buffer[start..end - 1],
-                token_name: output_vec[pre_match_found as usize],
-                index: start, length: end - 1 - start,
-                token_type: pre_match_found as usize
-            };
-            q.push_back(token);
-
-            start = end;
-            end += 1;
-            pre_match_found = -1;
-
-        }
-
-        for (i, reg) in regex_vec.iter().enumerate() {
-            if reg.is_match(&buffer[start..end]) {
-                match_found = i as i32;
-            }
-        }
-
-        if match_found == -1 && pre_match_found != -1 {
-            if need_keyword2 && pre_match_found != 4 {
-                q.pop_back();
-                eprintln!("Syntax error 3");
-                print_tokens(&q, &token);
-            }
-
-            if need_keyword2 && pre_match_found == 4 {
-                need_keyword2 = false;
-            }
-
-            let token = Token {
-                val: &buffer[start..end - 1],
-                token_name: output_vec[pre_match_found as usize],
-                index: start, length: end - 1 - start,
-                token_type: pre_match_found as usize
-            };
-            q.push_back(token);
-
-            if pre_match_found <= 2 && pre_match_found >= 0 {
-                need_keyword2 = true;
-            }
-
-            start = end - 1;
-        } else {
-            end += 1;
-        }
-
-        pre_match_found = match_found;
-        match_found = -1;
+    if should_print_tokens {
+        scanner::print_tokens(&tokens);
     }
-
-    if pre_match_found != -1 {
-        if need_keyword2 && pre_match_found != 4 {
-            q.pop_back();
-            eprintln!("Syntax error 5");
-            print_tokens(&q, &token);
-            return;
-        }
-
-        let token = Token {
-            val: &buffer[start..end - 1],
-            token_name: output_vec[pre_match_found as usize],
-            index: start, length: end - 1 - start,
-            token_type: pre_match_found as usize
-        };
-        q.push_back(token);
-    }
-
-    print_tokens(&q, &token);
 }
---
+++ src/scanner.rs
@@ -0,0 +1,134 @@
+use regex::Regex;
+use std::collections::VecDeque;
+use std::fmt::Write;
+
+#[derive(Debug)]
+pub struct Token<'a> {
+    val: &'a str,
+    token_name: &'a str,
+    index: usize,
+    length: usize,
+    token_type: usize
+}
+
+pub fn parse_tokens(buffer: &String) -> Result<VecDeque<Token>, &'static str> {
+    lazy_static! {
+        static ref KEYWORD1_RE: Regex = Regex::new(r"^((array)|(begin)|(boolean)|(break)|(do)|(else)|(end)|(enddo)|(endif)|(false)|(float)|(for)|(func)|(if)|(in)|(int)|(let)|(of)|(return)|(then)|(to)|(true)|(type)|(unit)|(var)|(while))$").unwrap();
+        static ref KEYWORD2_RE: Regex = Regex::new(r"^(,|:|;|\(|\)|\[|\]|\{|\}|\.|\+|-|\*|/|=|<|>|<>|<=|>=|&|\||:=)$").unwrap();
+        static ref ID_RE: Regex = Regex::new(r"^((_[A-Za-z_0-9]*([0-9A-Za-z])[A-Za-z_0-9]*)|(([A-Za-z])[0-9A-Za-z_]*))$").unwrap();
+        static ref INTLIT_RE: Regex = Regex::new(r"^(\d*)$").unwrap();
+        static ref FLOATLIT_RE: Regex = Regex::new(r"^(\d+\.\d*)$").unwrap();
+
+        static ref TOKEN_REGEXES: Vec<&'static Regex> = vec![&FLOATLIT_RE, &INTLIT_RE, &ID_RE, &KEYWORD1_RE, &KEYWORD2_RE];
+        static ref TOKEN_NAMES: Vec<&'static str> = vec!["floatlit", "intlit", "id", "keyword", "keyword"];
+    }
+
+    let (mut start, mut end) = (0, 1);
+    let mut match_found: i32 = -1;
+    let mut pre_match_found: i32 = -1;
+    let mut need_keyword2 = false;
+
+    let mut result: VecDeque<Token> = VecDeque::new();
+
+    while end <= buffer.len() {
+        if buffer[end - 1..end].eq(" ") {
+
+            if pre_match_found < 0 {
+                // print_tokens(&q, &token);
+                return Err("Syntax error 1");
+            }
+
+
+            if need_keyword2 && pre_match_found != 4 {
+                result.pop_back();
+                // print_tokens(&q, &token);
+                return Err("Syntax error 2");
+            }
+
+            if need_keyword2 && pre_match_found == 4 {
+                need_keyword2 = false;
+            }
+
+            let token = Token {
+                val: &buffer[start..end - 1],
+                token_name: TOKEN_NAMES[pre_match_found as usize],
+                index: start, length: end - 1 - start,
+                token_type: pre_match_found as usize
+            };
+            result.push_back(token);
+
+            start = end;
+            end += 1;
+            pre_match_found = -1;
+
+        }
+
+        for (i, reg) in TOKEN_REGEXES.iter().enumerate() {
+            if reg.is_match(&buffer[start..end]) {
+                match_found = i as i32;
+            }
+        }
+
+        if match_found == -1 && pre_match_found != -1 {
+            if need_keyword2 && pre_match_found != 4 {
+                result.pop_back();
+                // print_tokens(&q, &token);
+                return Err("Syntax error 3");
+            }
+
+            if need_keyword2 && pre_match_found == 4 {
+                need_keyword2 = false;
+            }
+
+            let token = Token {
+                val: &buffer[start..end - 1],
+                token_name: TOKEN_NAMES[pre_match_found as usize],
+                index: start, length: end - 1 - start,
+                token_type: pre_match_found as usize
+            };
+            result.push_back(token);
+
+            if pre_match_found <= 2 && pre_match_found >= 0 {
+                need_keyword2 = true;
+            }
+
+            start = end - 1;
+        } else {
+            end += 1;
+        }
+
+        pre_match_found = match_found;
+        match_found = -1;
+    }
+
+    if pre_match_found != -1 {
+        if need_keyword2 && pre_match_found != 4 {
+            result.pop_back();
+            // print_tokens(&q, &token);
+            return Err("Syntax error 5");
+        }
+
+        let token = Token {
+            val: &buffer[start..end - 1],
+            token_name: TOKEN_NAMES[pre_match_found as usize],
+            index: start, length: end - 1 - start,
+            token_type: pre_match_found as usize
+        };
+        result.push_back(token);
+    }
+
+    Ok(result)
+}
+
+pub fn print_tokens(queue: &VecDeque<Token>) {
+    let mut output: String = String::new();
+    for token in queue {
+        if token.token_name == "keyword" {
+            write!(&mut output, "{} ", &token.val).unwrap();
+        } else {
+            write!(&mut output, "{}:{} ", &token.val, &token.token_name).unwrap();
+        }
+    }
+    output = output.trim().to_string();
+    print!("{}", &output);
+}
