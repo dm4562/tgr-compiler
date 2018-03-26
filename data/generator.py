459c131db1cfde3ea62627d7aae2da97799887cc
Dhruv Mehra <dhruv.mehra4562@gmail.com>
2018-03-26 06:10:54 -0400

    Update generator script to create yacc grammar

--- data/generator.py
+++ data/generator.py
@@ -1,7 +1,9 @@
 import json
+import csv
 
 GRAMMAR_FILE = 'grammar.txt'
 JSON_FILE = 'grammar.json'
+TEMP_FILE = 'temp_grammar.txt'
 
 def generate_grammar():
     count = 1
@@ -41,9 +43,53 @@ def generate_grammar():
     with open(JSON_FILE, 'w') as out:
         json.dump(obj, out)
 
+    with open(TEMP_FILE, 'w') as temp:
+        with open(GRAMMAR_FILE) as file:
+            for line in file:
+                line = line.strip()
+                if line.startswith("=====") or line == "":
+                    break
+                line = line.split("->")
+                prod = line[1].strip()
+                prod = prod.split(" ")
+                out = []
+                for p in prod:
+                    if not p.isupper():
+                        out.append("\"{}\"".format(p))
+                    else:
+                        out.append(p)
+
+                prod = " ".join(out)
+                temp.write("{} : {} ;\n".format(line[0].strip(), prod))
+
+            temp.write("EPSILON : ;\n")
+
+def convert_ept_parse_table():
+    nonterminals = []
+    table = []
+    with open("ept.csv") as file:
+        reader = csv.reader(file)
+        first = next(reader)
+        # print(first)
+        for e in first[:-1]:
+            if e == 0:
+                continue
+
+            e = e.replace("\"", "")
+            nonterminals.append(e)
+
+        table.append([0 for _ in nonterminals])
+        # for row in reader:
+        #     # print(row)
+        #     jrow = [0]
+        #     for e in row[2:-1]:
+        #         if e
+
+        print(nonterminals)
 
 def main():
     generate_grammar()
+    # convert_ept_parse_table()
 
 if __name__ == '__main__':
     main()
