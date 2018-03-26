7a25764a7b440efa90056a148de178c7c83b905e
Dhruv Mehra <dhruv.mehra4562@gmail.com>
2018-03-26 03:12:29 -0400

    Write a script to generate grammar json

---
+++ data/generator.py
@@ -0,0 +1,49 @@
+import json
+
+GRAMMAR_FILE = 'grammar.txt'
+JSON_FILE = 'grammar.json'
+
+def generate_grammar():
+    count = 1
+    seen = { "EPSILON": ""}
+    non_terminals = [""]
+    productions = [""]
+    with open(GRAMMAR_FILE) as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("=====") or line == "":
+                break
+            line = line.split("->")
+            nt = line[0].strip()
+            prod = line[1].strip()
+            if nt not in seen:
+                seen[nt] = str(count)
+                count += 1
+                non_terminals.append(nt)
+            productions.append(prod)
+
+    final_productions = []
+    for prod in productions:
+        prod = prod.split(" ")
+        out = []
+        for e in prod:
+            if e in seen:
+                out.append(seen[e])
+            else:
+                out.append(e)
+        final_productions.append(out)
+
+    obj = {
+        "productions": final_productions,
+        "nonterminals": non_terminals
+    }
+
+    with open(JSON_FILE, 'w') as out:
+        json.dump(obj, out)
+
+
+def main():
+    generate_grammar()
+
+if __name__ == '__main__':
+    main()
